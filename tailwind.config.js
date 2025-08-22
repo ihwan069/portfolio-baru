7966ba3b14751e272b2972b2239dae66bd60713c
ihwan069 <Adliyyihwan@gmail.com>
2025-08-22 18:36:28 +0800

    commit file portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      colors: {
+        primary: "#ff7849",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+    },
+  },
+  plugins: [],
+};
